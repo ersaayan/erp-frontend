a320fd9f4a18c804fc1d23b75a302fed023d30b0
Eren Ali Şavk <esavk22@posta.pau.edu.tr>
2024-12-08 15:43:35 +0300

    Remove PurchaseInvoice components and related types for cleanup and refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,6 @@ import BankOperations from "@/components/BankOperations";
 import PosOperations from "@/components/PosOperations";
 import CurrentTransactions from "@/components/CurrentTransactions";
 import ApiDocumentation from "@/components/Settings/ApiDocumentation";
-import PurchaseInvoice from "@/components/PurchaseInvoice";
 import UsersPage from "@/components/users";
 import { useRouter } from "next/navigation";
 import { useToast } from "@/hooks/use-toast";
@@ -228,13 +227,6 @@ export default function Home() {
               {activeTab === "Banka İşlemleri" && <BankOperations />}
               {activeTab === "POS İşlemleri" && <PosOperations />}
               {activeTab === "Tanımlar" && <ApiDocumentation />}
-              {activeTab === "Alış Faturası" && (
-                <PurchaseInvoice
-                  current={JSON.parse(
-                    localStorage.getItem("currentPurchaseInvoice") || "{}"
-                  )}
-                />
-              )}
               {activeTab === "Kullanıcılar" && <UsersPage />}
             </TabContainer>
           </main>
