f76dc865bb6d951e0028e6281e73f821ef1d9162
ersaayan <ersaayan@gmail.com>
2024-12-14 15:09:12 +0300

    Update KarekodYazdir component: simplify EPL command structure and enhance print command for better barcode output

--- components/barcode/index.tsx
+++ components/barcode/index.tsx
@@ -57,14 +57,12 @@ export default function KarekodYazdir() {
 
       // EPL komutları
       const command = `
-^XA
-
-
-^FXfield for the element 'Sample Text Element'
-^FO16,16,2
-^FWN
-^A40,40^FDI'm a text element!^FS
-^XZ`;
+    N
+    q640
+    Q320,0
+    A16,16,0,3,1,1,N,"Stok Kodu: ${stokKodu}"
+    P${adet}
+    `;
       await qz.print(config, [
         { type: "raw", format: "command", data: command, flavor: "plain" },
       ]);
