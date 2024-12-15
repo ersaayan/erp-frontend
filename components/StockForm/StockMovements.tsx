ddad82616685499e226a78aa8fa01e7317ba5a04
ersaayan <ersaayan@gmail.com>
2024-12-16 02:16:12 +0300

    Wrap StockMovements DataGrid in Card component for improved layout

--- components/StockForm/StockMovements.tsx
+++ components/StockForm/StockMovements.tsx
@@ -17,6 +17,7 @@ import DataGrid, {
 } from "devextreme-react/data-grid";
 import { Alert, AlertDescription } from "@/components/ui/alert";
 import { AlertCircle, Loader2 } from "lucide-react";
+import { Card } from "../ui/card";
 
 interface StockMovement {
   id: string;
@@ -108,75 +109,81 @@ const StockMovements: React.FC<StockMovementsProps> = ({ productCode }) => {
   }
 
   return (
-    <DataGrid
-      dataSource={movements}
-      showBorders={true}
-      showRowLines={true}
-      showColumnLines={true}
-      rowAlternationEnabled={true}
-      allowColumnReordering={true}
-      allowColumnResizing={true}
-      columnAutoWidth={true}
-      wordWrapEnabled={true}
-      height="calc(100vh - 400px)"
-    >
-      <StateStoring
-        enabled={true}
-        type="localStorage"
-        storageKey="stockMovementsGrid"
-      />
-      <LoadPanel enabled={true} />
-      <Selection mode="multiple" />
-      <FilterRow visible={true} />
-      <HeaderFilter visible={true} />
-      <SearchPanel visible={true} width={240} placeholder="Ara..." />
-      <ColumnChooser enabled={true} mode="select" />
-      <Scrolling mode="virtual" />
-      <Paging enabled={false} />
-
-      <Column
-        dataField="createdAt"
-        caption="Tarih"
-        dataType="datetime"
-        format="dd.MM.yyyy HH:mm"
-      />
-      <Column dataField="documentNo" caption="Belge No" />
-      <Column dataField="documentType" caption="Belge Tipi" />
-      <Column dataField="invoiceType" caption="Fatura Tipi" />
-      <Column dataField="movementType" caption="Hareket Tipi" />
-      <Column dataField="currentCode" caption="Cari Kodu" />
-      <Column dataField="warehouseCode" caption="Depo" />
-      <Column dataField="description" caption="Açıklama" />
-      <Column
-        dataField="quantity"
-        caption="Miktar"
-        dataType="number"
-        format="#,##0.##"
-      />
-      <Column dataField="unitOfMeasure" caption="Birim" />
-      <Column
-        dataField="unitPrice"
-        caption="Birim Fiyat"
-        dataType="number"
-        format="#,##0.00"
-      />
-      <Column
-        dataField="totalPrice"
-        caption="Toplam"
-        dataType="number"
-        format="#,##0.00"
-      />
-      <Column dataField="branchCode" caption="Şube" />
-
-      <Summary>
-        <TotalItem column="quantity" summaryType="sum" valueFormat="#,##0.##" />
-        <TotalItem
-          column="totalPrice"
-          summaryType="sum"
-          valueFormat="#,##0.00"
+    <Card className="mt-4 p-4 h-full">
+      <DataGrid
+        dataSource={movements}
+        showBorders={true}
+        showRowLines={true}
+        showColumnLines={true}
+        rowAlternationEnabled={true}
+        allowColumnReordering={true}
+        allowColumnResizing={true}
+        columnAutoWidth={true}
+        wordWrapEnabled={true}
+        height="calc(100vh - 400px)"
+      >
+        <StateStoring
+          enabled={true}
+          type="localStorage"
+          storageKey="stockMovementsGrid"
         />
-      </Summary>
-    </DataGrid>
+        <LoadPanel enabled={true} />
+        <Selection mode="multiple" />
+        <FilterRow visible={true} />
+        <HeaderFilter visible={true} />
+        <SearchPanel visible={true} width={240} placeholder="Ara..." />
+        <ColumnChooser enabled={true} mode="select" />
+        <Scrolling mode="virtual" />
+        <Paging enabled={false} />
+
+        <Column
+          dataField="createdAt"
+          caption="Tarih"
+          dataType="datetime"
+          format="dd.MM.yyyy HH:mm"
+        />
+        <Column dataField="documentNo" caption="Belge No" />
+        <Column dataField="documentType" caption="Belge Tipi" />
+        <Column dataField="invoiceType" caption="Fatura Tipi" />
+        <Column dataField="movementType" caption="Hareket Tipi" />
+        <Column dataField="currentCode" caption="Cari Kodu" />
+        <Column dataField="warehouseCode" caption="Depo" />
+        <Column dataField="description" caption="Açıklama" />
+        <Column
+          dataField="quantity"
+          caption="Miktar"
+          dataType="number"
+          format="#,##0.##"
+        />
+        <Column dataField="unitOfMeasure" caption="Birim" />
+        <Column
+          dataField="unitPrice"
+          caption="Birim Fiyat"
+          dataType="number"
+          format="#,##0.00"
+        />
+        <Column
+          dataField="totalPrice"
+          caption="Toplam"
+          dataType="number"
+          format="#,##0.00"
+        />
+        <Column dataField="branchCode" caption="Şube" />
+
+        <Summary>
+          <TotalItem
+            column="quantity"
+            summaryType="sum"
+            valueFormat="#,##0.##"
+          />
+          <TotalItem
+            column="totalPrice"
+            summaryType="sum"
+            valueFormat="#,##0.00"
+          />
+        </Summary>
+      </DataGrid>
+    </Card>
   );
 };
 
