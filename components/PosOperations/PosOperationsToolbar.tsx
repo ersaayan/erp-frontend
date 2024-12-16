ac7e0923d714384a4aaeac2f2d3dd3d2b37a0c3e
ersaayan <ersaayan@gmail.com>
2024-12-16 16:38:21 +0300

    Remove unnecessary whitespace in PosOperationsToolbar component

--- components/PosOperations/PosOperationsToolbar.tsx
+++ components/PosOperations/PosOperationsToolbar.tsx
@@ -11,7 +11,6 @@ interface PosOperationsToolbarProps {
   onShowAllMovements: () => void;
   selectedPos: Pos | null;
 }
-
 const PosOperationsToolbar: React.FC<PosOperationsToolbarProps> = ({
   onShowAllMovements,
   selectedPos,
