88d84c37a7ce58b5ee7737d79e7ad4359a86ff4f
ersaayan <ersaayan@gmail.com>
2024-12-24 17:00:17 +0300

    Fix price list item selection logic in ProductSearch component to reference priceList.id

--- components/QuickSales/ProductSearch.tsx
+++ components/QuickSales/ProductSearch.tsx
@@ -112,7 +112,7 @@ const ProductSearch: React.FC<ProductSearchProps> = ({
   const handleProductSelect = (product: any) => {
     // Find price list item matching customer's price list
     const priceListItem = product.stockCardPriceLists.find(
-      (pl: any) => pl.id === customer?.priceList?.id
+      (pl: any) => pl.priceList.id === customer?.priceList?.id
     );
 
     if (!priceListItem) {
